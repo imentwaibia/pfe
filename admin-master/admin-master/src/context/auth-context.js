d71921fd557d21d1337e22df65dc5cd1ee3264ed
imentwaibia <90285080+imentwaibia@users.noreply.github.com>
2021-11-18 21:37:37 +0100

    Delate admin-master/admin-master directory

--- admin-master/admin-master/src/context/auth-context.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const Authcontext = createContext({
-  userId: null,
-  user:null,
-  token: null,
-  login: () => {},
-  logout: () => {}
-});
--- admin-master/admin-master/src/models/error-model.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import {Alert} from "react-bootstrap";
-
-export default function ErrorModel(props) {
-  return <>{props.error && <Alert variant="danger">{props.error}</Alert>}</>;
-}
